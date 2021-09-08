041752c8ce680984fed3fa798b1f7c3e61fa1e4d
Sergey Aristov <aristov775@gmail.com>
2021-09-08 10:54:27 +0300

    Добавлен дата класс данных ApodNW Создан метод получения картинки дня по запросу

--- app/src/main/java/ru/marslab/casespace/data/retrofit/NasaApi.kt
+++ app/src/main/java/ru/marslab/casespace/data/retrofit/NasaApi.kt
@@ -1,5 +1,14 @@
 package ru.marslab.casespace.data.retrofit
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.marslab.casespace.data.model.ApodNW
+
 interface NasaApi {
 
+    @GET("planetary/apod")
+    suspend fun getImageOfDay(
+        @Query("date") date: String
+    ): Response<ApodNW>
 }
