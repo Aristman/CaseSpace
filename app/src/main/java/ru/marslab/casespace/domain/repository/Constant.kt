56961f9abcb4d0e2a4c658776683ea6a594caf27
Sergey Aristov <aristov775@gmail.com>
2021-09-08 08:42:30 +0300

    Добавлен навигационнай граф. Создан объект для хранения констант Создан интерфейс локального хранилища.

---
+++ app/src/main/java/ru/marslab/casespace/domain/repository/Constant.kt
@@ -0,0 +1,7 @@
+package ru.marslab.casespace.domain.repository
+
+object Constant {
+    private const val BASE_API_PATH = "https://api.nasa.gov"
+
+    fun getBaseApiPath(): String = BASE_API_PATH
+}
---
+++ app/src/main/java/ru/marslab/casespace/domain/repository/Storage.kt
@@ -0,0 +1,5 @@
+package ru.marslab.casespace.domain.repository
+
+interface Storage {
+    fun getNasaApikey(): String
+}
