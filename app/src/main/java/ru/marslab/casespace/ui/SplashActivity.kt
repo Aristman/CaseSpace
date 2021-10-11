a89d1862500211f7d2c5593705c475a1d5d9b684
Sergey Aristov <aristov775@gmail.com>
2021-10-11 08:24:58 +0300

    На заставку добавлена анимация

--- app/src/main/java/ru/marslab/casespace/ui/SplashActivity.kt
+++ app/src/main/java/ru/marslab/casespace/ui/SplashActivity.kt
@@ -1,12 +1,12 @@
 package ru.marslab.casespace.ui
 
+import android.animation.Animator
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import ru.marslab.casespace.R
 
 private const val START_DELAY = 5000L
@@ -16,11 +16,24 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        lifecycleScope.launch {
-            delay(START_DELAY)
-            val mainIntent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(mainIntent)
-            finish()
-        }
+        findViewById<ImageView>(R.id.splash_image).animate()
+            .rotationXBy(750f)
+            .rotationYBy(750f)
+            .setInterpolator(LinearInterpolator())
+            .setDuration(START_DELAY)
+            .setListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(p0: Animator?) {}
+
+                override fun onAnimationEnd(p0: Animator?) {
+                    val mainIntent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(mainIntent)
+                    finish()
+                }
+
+                override fun onAnimationCancel(p0: Animator?) {}
+
+                override fun onAnimationRepeat(p0: Animator?) {}
+
+            })
     }
 }
